421bbc6798ed7a30329f2c47b0b9147e5291c71b
zoeschmitt <zoeschmitt@hotmail.com>
2023-06-04 21:37:47 -0500

    fix: deployment script updated to deploy all functions

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-FUNCTION_NAME_1=conversations
 
-echo "deploy cloud functions\n"
-gcloud functions deploy ${FUNCTION_NAME_1} --trigger-http --runtime=nodejs18 --source dist/${FUNCTION_NAME_1} --env-vars-file .env.yaml
+ROOT_FOLDER="dist/"
+
+echo "beginning functions deployment"
+
+# loop through the files
+for filepath in $(find "$ROOT_FOLDER" -type f -name "*.js"); do
+  # extract the function name from the file path
+  filename=$(basename "$filepath")
+  function_name="${filename%.*}"
+
+  echo "deploying $function_name"
+
+  # deploy the function
+  gcloud functions deploy "$function_name" \
+    --trigger-http \
+    --runtime=nodejs18 \
+    --source "dist/$function_name" \
+    --env-vars-file .env.yaml
+
+  echo "$function_name done"
+
+done
