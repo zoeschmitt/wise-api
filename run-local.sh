136034b5b16ab9e81e083939c5e6050220b3ebe5
zoeschmitt <zoeschmitt@hotmail.com>
2023-06-05 21:54:54 -0500

    fix: npm watch fix, now updating and redeploying locally with changes

--- run-local.sh
+++ run-local.sh
@@ -16,4 +16,4 @@ function parse_yaml {
    }'
 }
 
-bash -c "$(parse_yaml .env.yaml) functions-framework --source dist/conversations --target=conversations"
+bash -c "$(parse_yaml .env.yaml) functions-framework --source dist/conversations/get/get-conversations.js --target=getConversations"
