875184fe119d86cdbd1bb8acde3c499afcd5fbbc
zoeschmitt <zoeschmitt@hotmail.com>
2023-06-17 17:18:21 -0500

    feat: db client util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
+import { Client } from "pg";
 
 export const openAiAPI = (): OpenAIApi => {
   const { OPENAI } = process.env;
@@ -6,4 +7,15 @@ export const openAiAPI = (): OpenAIApi => {
     apiKey: OPENAI,
   });
   return new OpenAIApi(configuration);
+};
+
+export const dbClient = (): Client => {
+  const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+  return new Client({
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+  });
 };
