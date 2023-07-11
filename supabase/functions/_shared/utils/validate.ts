7d436227145380866de1cfd61166ab4e02fac63e
zoeschmitt <zoeschmitt@hotmail.com>
2023-07-10 20:54:51 -0500

    fix: validation checks params and body depending on request and schema

--- supabase/functions/_shared/utils/validate.ts
+++ supabase/functions/_shared/utils/validate.ts
@@ -1,5 +1,19 @@
 import { AnyObject, ObjectSchema, ValidationError } from "yup";
 import { ErrorCodes, apiError } from "./errors.ts";
+import { RequestMethod } from "../models/requests.ts";
+
+class CompleteRequest {
+  params?: Record<string, any>;
+  body?: Record<string, any>;
+
+  constructor(params?: Record<string, any>, body?: Record<string, any>) {
+    this.params = params ?? {};
+    this.body = body ?? {};
+  }
+}
+
+export const getRequestParams = (req: Request) =>
+  Object.fromEntries([...new URL(req.url).searchParams]);
 
 export const validate =
   <T extends AnyObject>(
@@ -8,9 +22,18 @@ export const validate =
   ) =>
   async (req: Request) => {
     try {
-      const body = await req.json();
-      await schema.validate(body);
-      return handler(req);
+      const { method } = req;
+      const request = new CompleteRequest();
+
+      request.params = getRequestParams(req)
+
+      if (method !== RequestMethod.GET) {
+        request.body = await req.json();
+      }
+
+      await schema.validate(request);
+
+      return await handler(req);
     } catch (err) {
       console.log(err);
       return err instanceof ValidationError
