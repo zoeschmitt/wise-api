52a7b1d3c64a77c63c9a2fc9b036df0cff9e1245
zoeschmitt <zoeschmitt@hotmail.com>
2023-08-05 17:47:33 -0500

    fix: added chats property to conversation model

--- supabase/functions/_shared/models/conversations.ts
+++ supabase/functions/_shared/models/conversations.ts
@@ -1,7 +1,10 @@
+import { Chat } from "./chats.ts";
+
 export type Conversation = {
   conversationId: string;
   userId: string;
   title: string;
   created: string;
   updated: string;
+  chats: Chat[];
 };
