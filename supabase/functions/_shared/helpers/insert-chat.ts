351cda2785e5eb510505864d9aa44819f30a2fc7
zoeschmitt <zoeschmitt@hotmail.com>
2023-08-07 22:58:26 -0500

    fix: returning most recent chat

--- supabase/functions/_shared/helpers/insert-chat.ts
+++ supabase/functions/_shared/helpers/insert-chat.ts
@@ -19,15 +19,19 @@ import { getInsertProperties, getInsertValues } from "../utils/db.ts";
 export const insertChat = async (
   chat: Chat,
   client: PoolClient
-): Promise<void> => {
+): Promise<Chat> => {
   const insertProps = getInsertProperties(chat);
   const insertVals = getInsertValues(insertProps);
 
   const query = `
   INSERT INTO chats (${insertProps.join(", ")})
-  VALUES (${insertVals.join(", ")})`;
+  VALUES (${insertVals.join(", ")})
+  RETURNING *
+  `;
 
   const values = insertProps.map((p) => chat[p as keyof Chat]);
 
-  await client.queryArray<any[]>(query, values);
+  const result = await client.queryObject<Chat>(query, values);
+
+  return result.rows[0];
 };
