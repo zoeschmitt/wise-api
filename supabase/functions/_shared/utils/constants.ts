428c5a5d8005382284bb15d92d41aa9b63b66c4c
zoeschmitt <zoeschmitt@hotmail.com>
2023-11-18 17:40:23 -0600

    fix: returning conversation id in message header

--- supabase/functions/_shared/utils/constants.ts
+++ supabase/functions/_shared/utils/constants.ts
@@ -4,8 +4,10 @@ export const PREP_KEYS = {
   FETCH_CONVERSATION: "fetch-conversation",
 };
 
-export const OPEN_AI_BASE_URL = "https://api.openai.com/v1"
+export const OPEN_AI_BASE_URL = "https://api.openai.com/v1";
 
 export const OPEN_AI_URLS = {
   chatCompletion: `${OPEN_AI_BASE_URL}/chat/completions`,
 };
+
+export const WISE_CONVERSATION_HEADER = "X-CONVERSATION-ID";
