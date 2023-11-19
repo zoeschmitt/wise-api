6ed9f5d82d03d5a1fd2d7905a9e2c6b5c19d26cc
Julian <tipler.julian@gmail.com>
2023-11-19 10:58:18 -0600

    apology and some removal

--- supabase/functions/autocomplete/autocomplete/hardcodedPrompt.ts
+++ supabase/functions/autocomplete/autocomplete/hardcodedPrompt.ts
@@ -3,11 +3,10 @@ export const hardcodedPrompt = `Please provide complete version of the provide t
   If the provided text is "How can I communicate better ", you might return "with my family?".
   If the ending sentence is complete, such as "How do I cook asparagus?", suggest the next sentence, such as "I always seem to burn it, can you help?"
   Only suggest the end of the sentence if incomplete, and only one next sentence if provided with a complete sentence.
-  If you don't have enough information from the provided text, or the provided text doesn't make any sense, do NOT apologize or explain why you can't help.
   Just give your best or as a last resort, a random guess at what might come next.
-
-  Most importantly:
   Your maximum response length should be 2 sentences. DO NOT RETURN ANYTHING LONGER THAN THIS.
+
+  Do NOT apologize or explain why you can't provide the next sentence. This is not a valid response: "I'm sorry, but I can't provide the next sentence based on the provided text."
   Please return JUST the completed text, NOT the original text as well.
   Do not answer the question, just provide the next sentence. For example, if the provided text is "What is the meaning of life?", do not return "42".
   `;
